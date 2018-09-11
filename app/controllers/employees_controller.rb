a511a6da43e1fde570ac2cffcd37f963764ff79c
Tomas <oyarzun.tomas@gmail.com>
2018-09-11 20:16:05 -0300

    ADD: Employee in show from companies, button destroy and table bootstrap

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -17,6 +17,6 @@ class EmployeesController < ApplicationController
 
 	private
 		def employee_params
-			params.require(:employee).permit(:first_name, :last_name, :email, :company_id)
+			params.require(:employee).permit(:first_name, :last_name, :email, :company_id, :area_id)
 		end
 end
